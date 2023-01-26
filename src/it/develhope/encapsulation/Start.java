c9f414c3c44aea8ebfeeb039ece2f5ef55f6b86e
lulanoise <lucrezia.rumore98@gmail.com>
2023-01-26 17:05:29 +0100

    first commit

--- src/it/develhope/encapsulation/Start.java
+++ src/it/develhope/encapsulation/Start.java
@@ -1,19 +1,31 @@
 package it.develhope.encapsulation;
-
-import java.util.Arrays;
 import java.util.Scanner;
-
 public class Start {
     public static void main(String[] args) {
-        House h = new House();
-        h.setFloorsNumber(3);
-        h.setAddress("Paiolo Magico");
-        h.setResidentsNames(new String[]{"Harry","Hermione","Ginny","Ron"});
-        // h.setResidentsNames("Harry,Hermione,Ginny,Ron") (var args); //String array
+        House house = new House();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println(h);
-       // this is needed to consume the new line left-over after nextInt()
+        System.out.println("Enter the number of floors:");
+        int floorsNumber = scanner.nextInt();
+        house.setFloorsNumber(floorsNumber);
+
+        scanner.nextLine();
+
+        System.out.println("Enter the address:");
+        String address = scanner.nextLine();
+        house.setAddress(address);
+
+        System.out.println("Enter the residents names separated by a comma:");
+        String residentsNamesInput = scanner.nextLine();
+        String[] residentsNames = residentsNamesInput.split(",");
+        house.setResidentsNames(residentsNames);
+
+
+       System.out.println(String.format("This house has %d floors, is located at %s and is occupied by %s.",
+       house.getFloorsNumber(),
+       house.getAddress(),
+       String.join(", ",
+       house.getResidentsNames())));
 
     }
-
 }
